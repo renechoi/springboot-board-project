69eb73fdba12a12121b2c04f7294fad1166b436b
renechoi <renechoi90@gmail.com>
2023-03-15 15:13:02 +0900

    feat: implement board repository and db connection

---
+++ board/src/main/java/kosta/boot/board/domain/dto/Board.java
@@ -0,0 +1,15 @@
+package kosta.boot.board.domain.dto;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class Board {
+    private int no;
+    private String title;
+    private String content;
+    private int hit;
+    private LocalDateTime regDate;
+    private int userNo;
+    private String userName;
+}
