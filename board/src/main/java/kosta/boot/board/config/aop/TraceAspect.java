6ea9f15f58d9136cf6b2e1425cd8a28d0b7ac9de
renechoi <renechoi90@gmail.com>
2023-03-16 09:35:54 +0900

    feat: implement aop aspect and annotation(trace, retry)

---
+++ board/src/main/java/kosta/boot/board/config/aop/TraceAspect.java
@@ -0,0 +1,20 @@
+package kosta.boot.board.config.aop;
+
+import kosta.boot.board.config.annotation.Trace;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class TraceAspect {
+
+    @Before("@annotation(kosta.boot.board.config.annotation.Trace)")
+    public void doTrace(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    }
+}
