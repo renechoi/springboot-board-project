31d3c76a5e51de2a8c3e893e96d0e0d03fca5385
renechoi <renechoi90@gmail.com>
2023-03-16 15:42:16 +0900

    feat : implement trace aop

--- board/src/main/java/kosta/boot/board/config/aop/TraceAspect.java
+++ board/src/main/java/kosta/boot/board/config/aop/TraceAspect.java
@@ -1,8 +1,13 @@
 package kosta.boot.board.config.aop;
 
 import kosta.boot.board.config.annotation.Trace;
+import kosta.boot.board.config.trace.LogTrace;
+import kosta.boot.board.config.trace.TraceStatus;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -10,11 +15,36 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class TraceAspect {
 
-    @Before("@annotation(kosta.boot.board.config.annotation.Trace)")
-    public void doTrace(JoinPoint joinPoint) {
-        Object[] args = joinPoint.getArgs();
-        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+    private final LogTrace logTrace;
+
+//
+//    @Before("@annotation(kosta.boot.board.config.annotation.Trace)")
+//    public void doTrace(JoinPoint joinPoint) {
+//        Object[] args = joinPoint.getArgs();
+//        log.info("[trace] {} args={}", joinPoint.getSignature(), args);
+//    }
+
+
+
+
+    @Around("@annotation(kosta.boot.board.config.annotation.Trace)")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        TraceStatus status = null;
+
+        try {
+            String message = joinPoint.getSignature().toShortString();
+            status = logTrace.begin(message);
+
+            Object result = joinPoint.proceed();
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+
     }
 }
